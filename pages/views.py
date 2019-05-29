b0aa83db038607f7b5d6974f90f0f73d04e1a58b
Valerii <v.dzhunkovskyi@gmail.com>
2019-05-29 16:36:27 +0300

    added templates, changed urls, views settings

--- pages/views.py
+++ pages/views.py
@@ -2,9 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-def home_view(*args, **kwargs): # *args, **kwargs
-	return HttpResponse("<h1>Hello world</h1>")
+def home_view(request, *args, **kwargs): # *args, **kwargs
+	print(request.user)
+	return render(request, "home.html", {})
+
+def contact_view(request, *args, **kwargs): # *args, **kwargs
+	return render(request, "contact.html", {})
 
 
-def contact_view(*args, **kwargs): # *args, **kwargs
-	return HttpResponse("<h1>Contact page</h1>")
+def about_view(request, *args, **kwargs): # *args, **kwargs
+	dict_contest = {
+		"my_text": 'This is text',
+		"my_number": 1234,
+		"my_list": [123, 123134, 124]
+	}
+	return render(request, "about.html", dict_contest)
+
+
+def social_view(request, *args, **kwargs): # *args, **kwargs
+	return render(request, "social.html", {})
